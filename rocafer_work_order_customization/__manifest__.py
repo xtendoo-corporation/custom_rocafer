c55ea439d32b846f1314244e04c122e1534cd941
Gokac9 <theabraham9@gmail.com>
2024-04-08 10:58:45 +0200

    initials folders

---
+++ rocafer_work_order_customization/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Rocafer Work_order Custom",
+    "summary": "Customization for work-order",
+    "version": "17.0.1.0.0",
+    "category": "Products",
+    "author": "Manuel Calero, Salvador Gonzalez, Abraham Carrasco, Xtendoo",
+    "license": "LGPL-3",
+    "application": True,
+    "depends": [
+    ],
+    "data": [
+    ],
+    "installable": True,
+}
