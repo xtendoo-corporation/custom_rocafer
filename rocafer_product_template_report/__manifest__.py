567dc5480544ac0e46da4ec80237682a8d7f1d3b
Gokac9 <theabraham9@gmail.com>
2024-05-06 11:52:48 +0200

    add module rocafer_product_template_report

---
+++ rocafer_product_template_report/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Rocafer Custom Order Report",
+    "summary": "Customization for work-order report",
+    "version": "17.0.1.0.0",
+    "category": "Products",
+    "author": "Manuel Calero, Salvador Gonzalez, Abraham Carrasco, Xtendoo",
+    "license": "LGPL-3",
+    "application": True,
+    "depends": [
+        'product',
+        'rocafer_product_template',
+    ],
+    "data": [
+        "views/report_product_rocafer.xml",
+        "reports/rocafer_report.xml",
+    ],
+    "installable": True,
+}
