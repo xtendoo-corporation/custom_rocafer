0f4828730d7ff54264276136645f0674110a463b
GonzalezJimenezSalvador <salvador.gon.jim@gmail.com>
2024-07-26 11:16:57 +0200

    changes on report

--- rocafer_sale_order_allowed_product/models/product_template.py
+++ rocafer_sale_order_allowed_product/models/product_template.py
@@ -11,9 +11,4 @@ class ProductTemplate(models.Model):
         partner_id = self._context.get('restricted_partner_id')
         if partner_id:
             domain.append(('res_partner_id', '=', partner_id))
-
-        print("*"*50)
-        print("partner_id", partner_id)
-        print("*"*50)
-
         return super()._search(domain, offset, limit, order, access_rights_uid)
