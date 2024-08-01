a336432472a6f1029cc6de6aeac2d90ea752a333
GonzalezJimenezSalvador <salvador.gon.jim@gmail.com>
2024-08-01 08:34:25 +0200

    sync

---
+++ rocafer_mrp_workorder/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Rocafer Custom",
+    "summary": "Customization for work-order",
+    "version": "17.0.1.0.0",
+    "category": "Manufacturing",
+    "author": "Salvador Gonzalez Xtendoo",
+    "license": "LGPL-3",
+    "application": True,
+    "depends": [
+        'contacts',
+        'mrp',
+        'sale',
+    ],
+    "data": [
+        'views/mrp_workorder_view.xml',
+    ],
+    "installable": True,
+}
