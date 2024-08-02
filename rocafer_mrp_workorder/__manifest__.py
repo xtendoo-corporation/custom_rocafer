971dfc3c090afadfc70f813b60f4bd5a02d3c27a
GonzalezJimenezSalvador <salvador.gon.jim@gmail.com>
2024-08-02 11:59:21 +0200

    show dates from sales order

--- rocafer_mrp_workorder/__manifest__.py
+++ rocafer_mrp_workorder/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Rocafer Custom",
-    "summary": "Customization for work-order",
+    "summary": "Customization view for work-order",
     "version": "17.0.1.0.0",
     "category": "Manufacturing",
     "author": "Salvador Gonzalez Xtendoo",
