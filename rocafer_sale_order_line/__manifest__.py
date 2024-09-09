a5355f20c041d8c9068b3586499a5c58c7c70216
GonzalezJimenezSalvador <salvador.gon.jim@gmail.com>
2024-09-09 11:13:25 +0200

    now compute linear meter from sale order line

---
+++ rocafer_sale_order_line/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "rocafer_sale_order_line",
+    "summary": """Sales Order Line Custom""",
+    "version": "17.0.1.0.0",
+    "description": """Sales Order Line Custom""",
+    "company": "Xtendoo",
+    "website": "http://www.xtendoo.es",
+    "category": "Warehouse Management",
+    "depends": ["product", "rocafer_product_template_report"],
+    "license": "AGPL-3",
+    "data": [
+    ],
+    "installable": True,
+}
---
+++ rocafer_sale_order_line/models/sale_order_line.py
@@ -0,0 +1,34 @@
+from odoo import models, api, fields
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    assembly_figure_x_from_product_template = fields.Integer(
+        string='Assembly figure x',
+        related='product_id.assembly_figure_x',
+    )
+
+    printing_cylinder_size_from_product_template = fields.Float(
+        string='Printing cylinder size',
+        related='product_id.printing_cylinder_size',
+    )
+
+    tolerance_from_product_template = fields.Float(
+        string='Tolerance',
+        related='product_id.tolerance',
+    )
+
+    linear_meters = fields.Float(
+        string='Linear meters',
+        compute='_compute_meters',
+        store=True
+    )
+
+    @api.depends('assembly_figure_x_from_product_template', 'printing_cylinder_size_from_product_template', 'tolerance_from_product_template', 'product_uom_qty')
+    def _compute_meters(self):
+        self.linear_meters = 0
+        for record in self.filtered(lambda r: r.assembly_figure_x_from_product_template):
+            if (record.tolerance_from_product_template == 0):
+                record.linear_meters = (record.product_uom_qty / 1000 * record.printing_cylinder_size_from_product_template) / record.assembly_figure_x_from_product_template
+            else:
+                record.linear_meters = (record.product_uom_qty / 1000 * record.printing_cylinder_size_from_product_template) / record.assembly_figure_x_from_product_template * (1 + record.tolerance_from_product_template / 100)
